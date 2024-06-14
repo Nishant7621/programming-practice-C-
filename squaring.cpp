830607df979307e155d0c70135e31f50707a4670
Nishant7621 <168707776+Nishant7621@users.noreply.github.com>
2024-06-14 10:15:31 +0530

    Add files via upload

---
+++ squaring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+      int x;
+      cout<<"Enter a number:";
+      cin>>x;
+      cout<<"Here is the square:";
+      cout<<x*x;
+
+    return 0;
+}
